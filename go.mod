8e78248cb2cbb019f042f2d3b8f242db3df50c12
Olena Nesterenko <554miles@gmail.com>
2022-12-05 09:55:01 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Alena557miles/gowebserver
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0
---
+++ webservergo.go
@@ -0,0 +1,57 @@
+package webservergo
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type Controller interface {
+	RegisterRouter(*mux.Router)
+	RegisterActions()
+}
+
+func StartServer(controllers ...Controller) {
+	ctx := context.Background()
+	router := mux.NewRouter()
+	srv := &http.Server{
+		Addr:              `0.0.0.0:8080`,
+		ReadTimeout:       time.Millisecond * 200,
+		WriteTimeout:      time.Millisecond * 200,
+		IdleTimeout:       time.Second * 10,
+		ReadHeaderTimeout: time.Millisecond * 200,
+		Handler:           router,
+	}
+
+	for _, concontroller := range controllers {
+		concontroller.RegisterRouter(router)
+		concontroller.RegisterActions()
+	}
+
+	go func() {
+		log.Println(`Web Server started`)
+		err := srv.ListenAndServe()
+		if !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal(err)
+		}
+	}()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done,
+		syscall.SIGTERM,
+		syscall.SIGINT,
+	)
+	<-done
+
+	log.Println(`Web Server is shutting down`)
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(ctx, err)
+	}
+}
